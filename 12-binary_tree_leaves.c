9cbddcc2cec45bc1d8ff1e0ef0e1868b76bb5979
Ayomide Kayode <ayomidekay7@gmail.com>
2023-11-27 22:14:57 +0100

    Count the leaves in a binary tree.

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,10 +12,10 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	size_t left, right;
 
 	if (tree == NULL)
-		return 0; /* Return 0 if tree is NULL */
+		return (0); /* Return 0 if tree is NULL */
 
 	if (tree->left == NULL && tree->right == NULL)
-		return 1; /* Node is a leaf */
+		return (1); /* Node is a leaf */
 
 	left = binary_tree_leaves(tree->left);
 	right = binary_tree_leaves(tree->right);
