0f081b8c7526ce52372924589e1d465f0709c26d
Ayomide Kayode <ayomidekay7@gmail.com>
2023-11-27 21:19:55 +0100

    Calcultate Tree Height.

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,6 +3,8 @@
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: Height of the tree, 0 if tree is NULL
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
