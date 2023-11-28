ec75c7e77b4943872e45e7b881c6ee7156623bcc
Ayomide Kayode <ayomidekay7@gmail.com>
2023-11-28 13:29:09 +0100

    Refactored Tree Height help function for balance Factor of a Tree.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,13 +9,20 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left, right;
+
 	if (tree == NULL)
-		return (0); /* Return 0 if tree is NULL */
+		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	left = binary_tree_height(tree->left) + 1;
+	right = binary_tree_height(tree->right) + 1;
+
+	if (left > right)
+		return (left);
+	return (right);
 }
 
 /**
