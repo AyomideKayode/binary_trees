d6c122f8edebcbaa8d9628399d02b15daca5977c
Ayomide Kayode <ayomidekay7@gmail.com>
2023-11-28 13:21:17 +0100

    Modified code for Binary insert right.

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,12 +12,14 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *right_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *right_node;
 
 	if (parent == NULL)
 	{
 		return (NULL); /* insertion error if parent is NULL */
 	}
+
+	right_node = malloc(sizeof(binary_tree_t));
 
 	if (right_node == NULL)
 		return (NULL); /* allocation error */
@@ -25,7 +27,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	right_node->parent = parent;
 	right_node->n = value;
 	right_node->left = NULL;
-	right_node->right = NULL;
+	right_node->right = parent->right;
 
 	if (parent->right != NULL)
 	{
