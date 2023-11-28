45e7d5fd9b9407d7f7b31aaf87e52c4d4bef7257
Ayomide Kayode <ayomidekay7@gmail.com>
2023-11-28 13:40:40 +0100

    Refactored Tree Height help function for balance Factor of a Tree.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
+ * binary_tree_balance_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree
  *
  * Return: Height of the tree, 0 if tree is NULL
  */
 
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_balance_height(const binary_tree_t *tree)
 {
 	size_t left, right;
 
 	if (tree == NULL)
-		return (0);
+		return (-1);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
@@ -22,7 +22,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (left > right)
 		return (left);
-	return (right);
+	else
+		return (right);
 }
 
 /**
@@ -31,13 +32,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
  *
  * Return: Balance factor of the tree, 0 if tree is NULL
  */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height, right_height;
+
 	if (tree == NULL)
 		return (0); /* Return 0 if tree is NULL */
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	return ((int)(left_height - right_height));
+	return (left_height - right_height);
 }
